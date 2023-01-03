f552d5c610f87210acdd43801b53a027e57ba10d
LEales <102584475+LEales@users.noreply.github.com>
2023-01-03 11:19:20 +0100

    unicorns!!

--- index.js
+++ index.js
@@ -2,34 +2,50 @@ $(document).ready(function() {
     $.ajax({
         url: 'http://localhost:5008/v1/unicorns/'
     }).done(function(result) {
-        getKids(result)
-    })
-})
-
-function getKids(result) {
-    let kids = $('#unicorn').children()
-    console.log(kids)
-    let num = Math.min(4, result.length)
-    for (let i = 0; i < num; i++) {
-        getID(result[i]['id'], i, kids)
-    }
-}
-
-
-function getID(index,i, kids) {
-    $.ajax({
-        url: 'http://localhost:5008/v1/unicorns/' + index,
-        success: function(result) {
-            console.log(result)
-            kids[i].children[0].children.attr('src', result['image'])
-            kids[i].children[1].children.html(result['name'])
-            kids[i].children[2].children.html(result['description'])
-            kids[i].children[3].children[0].html(result['spottedWhere']['name'])
-            kids[i].children[3].children[1].html(result['reportedBy'])
+        appendPost(result[0]['id'])
+        for (let i = 1; i < result.length; i++) {
+            appendAll(result[i]['id'])
         }
     })
+
+
+  
+
+})
+
+
+function appendPost(i) {
+    $.ajax({
+        url: 'http://localhost:5008/v1/unicorns/' + i
+    }).done(function(result) {
+       $('#postImg').attr('src', result['image'])
+       $('#postName').html(result['name'])
+       $('#postDesc').html(result['description'])
+
+    })
+    
+
 }
-    
 
+function appendAll(i) {
+    let source = document.getElementById("test"),
+    destination = document.getElementById("unicorn");
+    let evilclone = source.cloneNode(true);
+    evilclone.removeAttribute("id");
+    $.ajax({
+        url: 'http://localhost:5008/v1/unicorns/' + i
+    }).done(function(result) {
+        let kids = evilclone.childNodes
+        let img = kids[1].childNodes[1]
+        img.setAttribute('src', result['image'])
+        kids[3].childNodes[1].innerHTML = result['name']
+        kids[5].childNodes[1].innerHTML = result['description']
+        
+         
+       //$('#postImg').attr('src', result['image'])
+       //$('#postName').html(result['name'])
+       //$('#postDesc').html(result['description'])
 
-    
+    })
+    destination.appendChild(evilclone);
+}
