7eccf4cce3c2c664b2e307d09d45c252a1035912
StefanYousif <Stefan.nseriani@gmail.com>
2023-01-03 09:53:56 +0100

    ...
    ...

--- main.js
+++ main.js
@@ -37,6 +37,23 @@ function() {
     }
 }
 )*/
+// Initiate an Ajax request on button click
+$(document).on("click", "button", function(){
+    // Adding timestamp to set cache false
+    $.get("/examples/php/customers.php?v="+ $.now(), function(data){
+        $("body").html(data);
+    });       
+});
+
+// Add remove loading class on body element depending on Ajax request status
+$(document).on({
+    ajaxStart: function(){
+        $("body").addClass("loading"); 
+    },
+    ajaxStop: function(){ 
+        $("body").removeClass("loading"); 
+    }    
+});
 
 function postUnicorn() {
     return function() {
